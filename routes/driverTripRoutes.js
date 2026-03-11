18e480fffda2b2c43c05a0fbd16714c0f7a9dfa2
saqeeb66 <166957026+saqeeb66@users.noreply.github.com>
2026-03-11 20:04:21 +0530

    Update driverTripRoutes.js

--- routes/driverTripRoutes.js
+++ routes/driverTripRoutes.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const controller = require("../controllers/driverTripController");
 const auth = require("../middleware/jwtMiddleware");
 
+router.get("/", auth, controller.getActiveTrips);
+router.get("/completed", auth, controller.getCompletedTrips);
 router.post("/:tripId/start",auth,controller.startTrip);
 router.post("/:tripId/end",auth,controller.endTrip);
 
